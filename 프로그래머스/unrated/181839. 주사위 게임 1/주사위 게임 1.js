53016c0a0e1848313f7fd6ce6bb3306ed5ed9bc8
SeungHyun <120915990+plla2@users.noreply.github.com>
2024-01-05 15:03:50 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,7 +1,8 @@
 function solution(a, b) {
-    if(a%2===1 && b%2===1){
+    const isOdd=(num)=>num%2===1
+    if(isOdd(a) && isOdd(b) ){
         return Math.pow(a,2) + Math.pow(b,2);
-    }else if(a%2===1 || b%2===1){
+    }else if(isOdd(a) || isOdd(b)){
         return 2 * (a+b);
     }else {
         return Math.abs(a-b);
